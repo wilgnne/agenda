9cae9a351785bf0668aa8ffce7d0c591322bcb70
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-11-19 20:27:03 -0300

    feat: School subject controller

---
+++ Wilgnne.Agenda.Api/Controllers/SchoolSubjectController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Wilgnne.Agenda.Application.SchoolSubjects.Command.InsertSchoolSubjectCommand;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Wilgnne.Agenda.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolSubjectController(IMediator mediator) : ControllerBase
+    {
+
+        // POST api/<SchoolSubjectController>
+        [HttpPost]
+        public async Task<InsertSchoolSubjectCommandResponse> Post([FromBody] InsertSchoolSubjectCommand body)
+        {
+            return await mediator.Send(body);
+        }
+    }
+}

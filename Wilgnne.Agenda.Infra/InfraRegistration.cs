b91a83bb206bfe33764b16a2ca75877772843902
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-11-19 16:04:34 -0300

    feat: create infra project & OpenTelemetry registration

---
+++ Wilgnne.Agenda.Infra/InfraRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Wilgnne.Agenda.Infra
+{
+    public static partial class InfraRegistration
+    {
+        public static IServiceCollection AddInfraServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddOpenTelemetryService();
+        }
+
+        public static ILoggingBuilder AddInfraLoggings(this ILoggingBuilder logging)
+        {
+            return logging.AddOpenTelemetryLoggings();
+        }
+    }
+}

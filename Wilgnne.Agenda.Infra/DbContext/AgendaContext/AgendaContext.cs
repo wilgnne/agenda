6112968ac0df120ea3e84f33971f60321808a410
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-11-19 19:19:12 -0300

    chore: move AgendaContext & configure model creating

---
+++ Wilgnne.Agenda.Infra/DbContext/AgendaContext/AgendaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Wilgnne.Agenda.Infra.DbContext.AgendaContext
+{
+    public class AgendaContext(DbContextOptions<AgendaContext> options) : Microsoft.EntityFrameworkCore.DbContext(options)
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly(),
+                (type) => (type.Namespace ?? "").Contains("Configurations"));
+        }
+    }
+}

686b40b28996959a19b5b703dd7f838ac9bd1431
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-11-26 13:18:00 +0000

    feat: web project & basic firebase google login

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,20 @@
-const App = () => <p>Hello world</p>;
+import { useCallback } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "./config/firebase/auth.ts";
+
+const App = () => {
+  const handleGoogleSignIn = useCallback(async () => {
+    // Sign in using a popup.
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+
+    // The signed-in user info.
+    const user = result.user;
+
+    console.log({ user, token: await user.getIdToken() });
+  }, []);
+
+  return <button onClick={() => handleGoogleSignIn()}>Logar com google</button>;
+};
 
 export default App;

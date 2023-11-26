a74be8d4e9f262fb5744275f22db5455a75ed24c
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-11-26 23:32:29 +0000

    feat: date only & time only ef core converters

---
+++ backend/Wilgnne.Agenda.Infra/Extensions/DateOnlyConverter.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Wilgnne.Agenda.Infra.Extensions;
+
+public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+{
+    public DateOnlyConverter() : base(
+        dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+        dateTime => DateOnly.FromDateTime(dateTime))
+    { }
+}
